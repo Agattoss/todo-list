0849a30769c092d8db1b3cc3b75ec03c93adbe0c
Agata <108717863+Agattoss@users.noreply.github.com>
2022-10-23 12:42:32 +0200

    event binding

--- src/app/input-button-unit/input-button-unit.component.ts
+++ src/app/input-button-unit/input-button-unit.component.ts
@@ -3,18 +3,31 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-input-button-unit',
   template: `
-  <p>
-    input-button-unit works!
-    The title is: {{ title }}
-  </p>
-`,
+    <p>
+      input-button-unit works!
+      The title is: {{ title }}
+    </p>
+
+    <input
+      #inputElementRef
+      [value]="title"
+      (keyup)="changeTitle($event)">
+
+    <button (click)="changeTitle(inputElementRef.value)">
+      Save
+    </button>
+  `,
   styleUrls: ['./input-button-unit.component.scss']
 })
 export class InputButtonUnitComponent implements OnInit {
   title = 'Hello World';
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  changeTitle(event: any): void {
+    this.title = event.target.value
+  }
 }
